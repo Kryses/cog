b2b4e6862d8ae0157ce49f60e2997f2c0234c29f
Chad Provencher <thechad802@gmail>
2018-11-06 20:06:54 -0800

    added comments and notes mixin.

--- cog/settings.py
+++ cog/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'projects',
-    'pipelines'
+    'pipelines',
+    'assets',
+    'notes'
 ]
 
 MIDDLEWARE = [
--- notes/models.py
+++ notes/models.py
@@ -2,17 +2,34 @@ from django.db import models
 
 # Create your models here.
 from cog.base_classes.base_models import BaseModel, UserMixin
+from pipelines.models import StatusMixin
 from projects.models import ProjectMixin
 
 
-class NotesMixin(models.Model, UserMixin):
-    notes = models.ManyToManyField('notes.Notes', related_name='%(class)s_created_by', null=True)
+class NotesMixin(models.Model):
+    """Apply a notes field to any model."""
+    notes = models.ManyToManyField('notes.Notes', related_name='%(class)s_notes', null=True)
+
+    class Meta:
+        abstract = True
 
 
-class Note(BaseModel, ProjectMixin):
+class CommentsMixin(models.Model):
+    """Apply a notes field to any model."""
+    comments = models.ManyToManyField('notes.Comment', related_name='%(class)s_comments', null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Note(BaseModel, ProjectMixin, StatusMixin, UserMixin, CommentsMixin):
+    """A note that can be applied to an asset or, shot that can be used to start a discussion."""
+
     text = models.TextField()
 
 
 class Comment(BaseModel, UserMixin):
+    """A comment that can be create by a user in a discussion."""
+
     parent = models.ForeignKey('notes.Comment', on_delete=models.SET_NULL, null=True)
     text = models.TextField()
