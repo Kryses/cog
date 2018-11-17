09543adc383875c969dfc8d69d0d0eb2afa5be50
Chad Provencher <thechad802@gmail>
2018-11-17 12:04:17 -0800

    set python 3 alias.

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,4 @@
+alias python=/usr/bin/python3
 python ./manage.py makemigrations --settings "test_settings"
 python ./manage.py migrate --settings "test_settings"
 python ./manage.py test --settings "test_settings"
